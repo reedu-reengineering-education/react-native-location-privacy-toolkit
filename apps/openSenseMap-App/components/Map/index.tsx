8e47dc39a0f242573daaeb4f98fd7b82f093ebda
Felix Erdmann <hello@felixerdmann.com>
2023-02-27 11:38:44 +0100

    update deps

--- apps/openSenseMap-App/components/Map/index.tsx
+++ apps/openSenseMap-App/components/Map/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import Constants from "expo-constants";
 import MapLibreGL, { MapViewProps } from "@maplibre/maplibre-react-native";
 import { MAPTILER_KEY } from "@env";
@@ -7,16 +7,18 @@ import { MAPTILER_KEY } from "@env";
 // Required on Android. See Android installation notes.
 MapLibreGL.setAccessToken(null);
 
-const Map = (props: MapViewProps) => {
+const Map = forwardRef<MapLibreGL.MapView, MapViewProps>((props, ref) => {
   return (
     <MapLibreGL.MapView
+      ref={ref}
       style={{
         flex: 1,
       }}
       styleURL={`https://api.maptiler.com/maps/topo/style.json?key=${MAPTILER_KEY}`}
+      attributionPosition={{ right: 10, bottom: 10 }}
       {...props}
     />
   );
-};
+});
 
 export default Map;
--- apps/openSenseMap-App/features/boxes/components/OsemBoxes.tsx
+++ apps/openSenseMap-App/features/boxes/components/OsemBoxes.tsx
@@ -3,13 +3,16 @@ import { errorAtom } from "@/stores/errorStore";
 import { loadingAtom } from "@/stores/loadingStore";
 import flip from "@turf/flip";
 import { useAtom } from "jotai";
-import React from "react";
+import React, { forwardRef } from "react";
 import { useEffect } from "react";
 import { Text } from "react-native";
 import { useBoxes } from "../api";
-import MapLibreGL from "@maplibre/maplibre-react-native";
+import MapLibreGL, { ShapeSourceProps } from "@maplibre/maplibre-react-native";
 
-const OsemBoxes = () => {
+const OsemBoxes = forwardRef<
+  MapLibreGL.ShapeSource,
+  Omit<ShapeSourceProps, "id">
+>((_props, ref) => {
   const { data: boxes, error } = useBoxes({
     minimal: true,
   });
@@ -27,49 +30,53 @@ const OsemBoxes = () => {
 
   const lastWeek = new Date(new Date().setDate(new Date().getDate() - 7));
 
+  console.log(boxes.features.length);
+
   return (
     <MapLibreGL.ShapeSource
-      id="boxes"
+      id="senseBoxes"
+      ref={ref}
       shape={boxes}
       onPress={({ features, coordinates, point }) => {
         setSelectedBox(features[0] as senseBox);
       }}
+      {..._props}
     >
       <MapLibreGL.CircleLayer
-        id="boxes"
-        // style={{
-        //   circleColor: "#4eaf47",
-        //   circleRadius: ["interpolate", ["linear"], ["zoom"], 5, 4, 10, 15],
-        //   circleStrokeWidth: [
-        //     "interpolate",
-        //     ["linear"],
-        //     ["zoom"],
-        //     5,
-        //     0.2,
-        //     8,
-        //     2,
-        //   ],
-        //   circleStrokeColor: "#fff",
-        //   circleStrokeOpacity: [
-        //     "case",
-        //     ["<", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
-        //     0.3,
-        //     [">=", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
-        //     1,
-        //     0.3,
-        //   ],
-        //   circleOpacity: [
-        //     "case",
-        //     ["<", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
-        //     0.3,
-        //     [">=", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
-        //     1,
-        //     0.3,
-        //   ],
-        // }}
+        id="senseBoxes"
+        style={{
+          circleColor: "#4eaf47",
+          circleRadius: ["interpolate", ["linear"], ["zoom"], 5, 4, 10, 15],
+          circleStrokeWidth: [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            5,
+            0.2,
+            8,
+            2,
+          ],
+          circleStrokeColor: "#fff",
+          circleStrokeOpacity: [
+            "case",
+            ["<", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
+            0.3,
+            [">=", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
+            1,
+            0.3,
+          ],
+          circleOpacity: [
+            "case",
+            ["<", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
+            0.3,
+            [">=", ["get", "lastMeasurementAt"], lastWeek.toISOString()],
+            1,
+            0.3,
+          ],
+        }}
       ></MapLibreGL.CircleLayer>
     </MapLibreGL.ShapeSource>
   );
-};
+});
 
 export default OsemBoxes;
